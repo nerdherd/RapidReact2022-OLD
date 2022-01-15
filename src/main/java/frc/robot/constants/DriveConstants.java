71a7299e71b9bf3eac5ff4d2ca65804b1e45b412
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2022-01-15 13:22:58 -0800

    Add DriveConstants.java
    All values are set to 0.

---
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -0,0 +1,79 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.constants;
+
+/**
+ * Add your docs here.
+ */
+public class DriveConstants {
+    public static final double kLeftP = 0 * 0 / 0 * 0 * 0; //replace these
+	public static final double kLeftI = 0;
+	public static final double kLeftD = 0;
+
+	public static final double kRightP = 0 * 0 / 0 * 0 * 0; //2.12 3/31 for talon
+	public static final double kRightI = 0;
+    public static final double kRightD = 0;
+    
+    public static final double kDriveMaxVel = 0;
+    public static final double kDriveMaxAccel = 0;
+    
+    public static final double kGoalMetersY = 0;
+    public static final double kAutoLineMeters = 0;
+    public static final double kEnemyTrenchMetersX = 0;
+    public static final double kEnemyTrenchMetersY = 0;
+    
+    
+    public static final double kTrenchMetersX = 0;
+    public static final double kTrenchMetersY = 0;
+    public static final double kTrenchThirdBallX = 0;
+    public static final double kEndTrenchMetersX = 0;
+    public static final double kEndTrenchMetersY = 0;
+    
+    public static final double kLeftRamseteS = 0;
+    public static final double kLeftRamseteV = 0;
+    public static final double kLeftRamseteA = 0;
+
+
+    public static final double kramseteS = 0;
+    public static final double kramseteV = 0;
+    public static final double kramseteA = 0;
+
+    // public static final double kramseteP = 0;
+    // public static final double kramseteI = 0;
+    // public static final double kramseteD = 0;
+
+    public static final double kRightRamseteS = 0;
+    public static final double kRightRamseteV = 0;
+    public static final double kRightRamseteA = 0;
+
+	public static final double kRamseteMaxVolts = 0;
+
+    
+    
+    public static double kLeftStatic = 0; 
+    public static double kRightStatic =  0;
+
+    public static double kLeftF = 0;
+    public static double kRightF = 0;
+
+    public static int kMaxVelocity = 0;
+    public static double kLeftTicksPerFoot = (0 + 0) / 2;
+    public static double kRightTicksPerFoot = (0 + 0) / 2;
+
+    public static double kTrackWidth = 0;
+	public static final double kMaxCentripetalAcceleration = 0;
+    
+    public static final double kVLinear = 0;
+    public static final double kALinear = 0;
+    public static final double kVAngular = 0;
+    public static final double kAAngular = 0;
+    
+    public static final double gearReduction = 0;
+    public static final double wheelRadiusMeters = 0;
+    
+}
