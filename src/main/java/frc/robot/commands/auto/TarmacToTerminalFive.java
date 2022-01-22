d7c944013e8b4bbbb08b1e9a1a7b6ea87958e250
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2022-01-22 15:06:47 -0800

    Update five ball auto
    Split trajectories into two and add shooting commands

--- src/main/java/frc/robot/commands/auto/TarmacToTerminalFive.java
+++ src/main/java/frc/robot/commands/auto/TarmacToTerminalFive.java
@@ -18,11 +18,13 @@ import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.trajectory.constraint.CentripetalAccelerationConstraint;
 import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.intake.IntakeBalls;
+import frc.robot.commands.shooting.ShootBall;
 import frc.robot.constants.DriveConstants;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.RamseteController;
@@ -46,15 +48,31 @@ public class TarmacToTerminalFive extends SequentialCommandGroup {
         TrajectoryConfig config = new TrajectoryConfig(DriveConstants.kDriveMaxVel, DriveConstants.kDriveMaxAccel);
         config.addConstraints(List.of(autoVoltageConstraint, autoCentripetalAccelerationConstraint));
 
-        Trajectory tarmacToTerminal = TrajectoryGenerator.generateTrajectory(
+        Trajectory tarmacToMidBall = TrajectoryGenerator.generateTrajectory(
             new Pose2d(0, 91.37500, new Rotation2d(0.25)),
             List.of(new Translation2d(29.8244158, 150.06500),
                     new Translation2d(Math.cos(DriveConstants.kTarmacToBallOne) * 16.867, Math.sin(DriveConstants.kTarmacToBallOne) * 16.867)
             ),
+            new Pose2d(Math.cos(DriveConstants.kTarmacToBallOne) * 16.867, Math.sin(DriveConstants.kTarmacToBallOne) * 16.867, new Rotation2d(1.92)),
+            config);
+
+        Trajectory midBallToTerminal = TrajectoryGenerator.generateTrajectory(
+            new Pose2d(Math.cos(DriveConstants.kTarmacToBallOne) * 16.867, Math.sin(DriveConstants.kTarmacToBallOne) * 16.867, new Rotation2d(1.92)),
+            null, // No waypoints
             new Pose2d(275.24, 132.93, new Rotation2d(0.449923)),
             config);
 
-        RamseteCommand driveTarmacToTerminal = new RamseteCommand(tarmacToTerminal,
+        RamseteCommand driveTarmacToMidBall = new RamseteCommand(tarmacToMidBall,
+            m_drive::getPose2d,
+            new RamseteController(1.05, 0.14),
+            new SimpleMotorFeedforward(DriveConstants.kramseteS, DriveConstants.kramseteV, DriveConstants.kramseteA),
+            m_drive.m_kinematics,
+            m_drive::getCurrentSpeeds,
+            new PIDController(DriveConstants.kLeftP, DriveConstants.kLeftI, DriveConstants.kLeftD),
+            new PIDController(DriveConstants.kRightP, DriveConstants.kRightI, DriveConstants.kRightD),
+            m_drive::setVoltage, m_drive);
+
+        RamseteCommand driveMidBallToTerminal = new RamseteCommand(midBallToTerminal,
             m_drive::getPose2d,
             new RamseteController(1.05, 0.14),
             new SimpleMotorFeedforward(DriveConstants.kramseteS, DriveConstants.kramseteV, DriveConstants.kramseteA),
@@ -66,8 +84,12 @@ public class TarmacToTerminalFive extends SequentialCommandGroup {
 
         addCommands(
             new BasicAutoNoMove(),
-            new ParallelRaceGroup(new IntakeBalls(), driveTarmacToTerminal),
-            new WaitCommand(3)
+            new ParallelRaceGroup(new IntakeBalls(), driveTarmacToMidBall),
+            // turn to target
+            new ParallelRaceGroup(new ShootBall(), new WaitCommand(5)),
+            new ParallelRaceGroup(new IntakeBalls(), driveMidBallToTerminal),
+            // turn to target
+            new ParallelRaceGroup(new ShootBall(), new WaitCommand(5))
         );
 
     }
