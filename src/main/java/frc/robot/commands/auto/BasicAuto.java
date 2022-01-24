b28ec78eb4dc8d5e460e81f58a5a0bd3d70f0caa
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2022-01-24 15:34:10 -0800

    Update basic auto
    Now uses vision

--- src/main/java/frc/robot/commands/auto/BasicAuto.java
+++ src/main/java/frc/robot/commands/auto/BasicAuto.java
@@ -11,18 +11,23 @@ package frc.robot.commands.auto;
 import com.nerdherd.lib.drivetrain.auto.DriveTime;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Robot;
 import frc.robot.commands.intake.Stow;
 import frc.robot.commands.shooting.ShootBall;
+import frc.robot.commands.vision.MoveToAngleLime;
+import frc.robot.constants.VisionConstants;
 
 public class BasicAuto extends SequentialCommandGroup {
     
     public BasicAuto() {
         addCommands(
-            new ShootBall(),
-            new InstantCommand(() -> Robot.hood.setStoredAngle(), Robot.hood),
+            new ShootBall(), 
+            new InstantCommand(() -> Robot.hood.setStoredAngle(), Robot.hood), 
+            new ParallelRaceGroup(new WaitCommand(2), new MoveToAngleLime(VisionConstants.kRotP_lime, VisionConstants.kVecP_lime)),
+            new ParallelRaceGroup(new ShootBall(), new WaitCommand(8)),
             new DriveTime(Robot.drive, -0.2, 1),
             new Stow(),
             new WaitCommand(1),
