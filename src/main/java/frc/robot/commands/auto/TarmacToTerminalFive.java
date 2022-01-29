3796bbd8f926543272ae114110da6ac5646c58c6
Ayaka <akawano2005@gmail.com>
2022-01-29 09:54:02 -0800

    Five Ball Auto update
    Fix rotation 2d

--- src/main/java/frc/robot/commands/auto/TarmacToTerminalFive.java
+++ src/main/java/frc/robot/commands/auto/TarmacToTerminalFive.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.lang.Math;
 
+import com.nerdherd.lib.drivetrain.characterization.OpenLoopDrive;
 import com.nerdherd.lib.drivetrain.experimental.Drivetrain;
 
 import frc.robot.Robot;
@@ -54,17 +55,17 @@ public class TarmacToTerminalFive extends SequentialCommandGroup {
         config.addConstraints(List.of(autoVoltageConstraint, autoCentripetalAccelerationConstraint));
 
         Trajectory tarmacToMidBall = TrajectoryGenerator.generateTrajectory(
-            new Pose2d(0, 2.320925, new Rotation2d(0.25)),
-            List.of(new Translation2d(0.757540, 3.811651),
-                    new Translation2d(Math.cos(DriveConstants.kTarmacToBallOneAngle) * 0.428422, Math.sin(DriveConstants.kTarmacToBallOneAngle) * 0.428422)
+            new Pose2d(-0.175387, 2.320925, new Rotation2d(0.0261799388)),
+            List.of(new Translation2d(0.757540, 3.811651), // The angle between the two balls are very sharp so must adjust in some way (PID)
+                    new Translation2d(3.1736324132, 2.24290163814) 
             ),
-            new Pose2d(Math.cos(DriveConstants.kTarmacToBallOneAngle) * 0.428422, Math.sin(DriveConstants.kTarmacToBallOneAngle) * 0.428422, new Rotation2d(1.92)),
+            new Pose2d(3.1736324132, 2.24290163814, new Rotation2d(-3.31176226)),
             config);
 
         Trajectory midBallToTerminal = TrajectoryGenerator.generateTrajectory(
-            new Pose2d(Math.cos(DriveConstants.kTarmacToBallOneAngle) * 0.428422, Math.sin(DriveConstants.kTarmacToBallOneAngle) * 0.428422, new Rotation2d(1.92)),
+            new Pose2d(3.1736324132, 2.24290163814, new Rotation2d(2.97142305)),
             new ArrayList<Translation2d>(), // No waypoints
-            new Pose2d(6.991096, 3.376422, new Rotation2d(0.449923)),
+            new Pose2d(7.000748, 3.35661, new Rotation2d(0.763581548)), //Find way to turn to basket without hitting terminal and intaking both balls
             config);
 
         RamseteCommand driveTarmacToMidBall = new RamseteCommand(tarmacToMidBall,
@@ -88,12 +89,23 @@ public class TarmacToTerminalFive extends SequentialCommandGroup {
             m_drive::setVoltage, m_drive);
 
         addCommands(
-            new BasicAutoNoMove(),
+            // Without shooting/intake
+            // driveTarmacToMidBall,
+            // new WaitCommand(3),
+            // driveMidBallToTerminal
+
+
+            // With shooting/intake
+            new BasicAutoNoMove(), //Robot must face towards basket when starting unless shooting backwards
+            // Make sure intake and shooter are facing same way (if opposite, no need to face basket)
+            // Turn towards ball to intake
             new ParallelRaceGroup(new IntakeBalls(), driveTarmacToMidBall),
             new MoveToAngleLime(VisionConstants.kRotP_lime, VisionConstants.kVecP_lime),
             new ParallelCommandGroup(new InstantCommand(() -> Robot.hood.setAngle(45))),
             new ParallelRaceGroup(new ShootBall(), new WaitCommand(5)),
             new ParallelRaceGroup(new IntakeBalls(), driveMidBallToTerminal),
+            new ParallelRaceGroup(new OpenLoopDrive(Robot.drive, -0.2), new WaitCommand(1)),
+            // Turn towards basket to shoot
             new MoveToAngleLime(VisionConstants.kRotP_lime, VisionConstants.kVecP_lime),
             new ParallelCommandGroup(new InstantCommand(() -> Robot.hood.setAngle(45))),
             new ParallelRaceGroup(new ShootBall(), new WaitCommand(5))
