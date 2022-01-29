8db55ff026e9f15e1ba4ede7181e185bf5b4c9b2
Tiffany Chen <tchen1914@gmail.com>
2022-01-29 11:10:11 -0800

    More logging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,9 +7,16 @@ package frc.robot;
 import com.nerdherd.lib.drivetrain.teleop.TankDrive;
 import com.nerdherd.lib.motor.commands.ResetSingleMotorEncoder;
 import com.nerdherd.lib.motor.single.SingleMotorVictorSPX;
+import com.nerdherd.lib.motor.motorcontrollers.NerdySparkMax;
+import com.nerdherd.lib.motor.motorcontrollers.NerdyTalon;
+import com.nerdherd.lib.motor.motorcontrollers.NerdyFalcon;
 import com.nerdherd.lib.oi.AbstractOI;
 import com.nerdherd.lib.oi.controllers.NerdyXboxController.Hand;
 import com.nerdherd.lib.pneumatics.Piston;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -34,8 +41,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import edu.wpi.first.wpilibj.Timer;
 
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -47,7 +54,6 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   public static final String kDate = "2021_1_15_";
-  
   public static SingleMotorVictorSPX intakeRoll;
   private String m_filePath1 = "/media/sda1/logs/";
   private String m_filePath2 = "/home/lvuser/logs/";
@@ -55,7 +61,17 @@ public class Robot extends TimedRobot {
   public FileWriter m_writer;
   private boolean writeException = false;
   private double m_logStartTime = 0;
+
+  private static AHRS m_nav;
+
   public static ResetSingleMotorEncoder hoodReset;
+
+  public static NerdySparkMax leftMaster;
+  public static NerdySparkMax rightMaster;
+  public static NerdySparkMax leftFollower;
+  public static NerdySparkMax rightFollower;
+  public static NerdyFalcon elevator;
+  public static NerdyTalon arm;
 
   public static XboxOI xbox_oi;
   public static Drive drive;
@@ -86,6 +102,12 @@ public class Robot extends TimedRobot {
     intakeRoll = new SingleMotorVictorSPX(RobotMap.kIntakeRoll, "Intake Rollers", false);
     intake = new Piston(RobotMap.kIntakePort1, RobotMap.kIntakePort2);
     hoodReset = new ResetSingleMotorEncoder(Robot.hood);
+    leftMaster = new NerdySparkMax(RobotMap.kLeftMasterID, MotorType.kBrushless);
+    rightMaster = new NerdySparkMax(RobotMap.kRightMasterID, MotorType.kBrushless);
+    leftFollower = new NerdySparkMax(RobotMap.kLeftFollower1ID, MotorType.kBrushless);
+    rightFollower = new NerdySparkMax(RobotMap.kRightFollower1ID, MotorType.kBrushless);
+    elevator = new NerdyFalcon(RobotMap.kClimberID1);
+    arm = new NerdyTalon(RobotMap.kClimberID2);
 
     drive.setDefaultCommand(new TankDrive(Robot.drive, Robot.xbox_oi));
     drive.configKinematics(DriveConstants.kTrackWidth, new Rotation2d(0), new Pose2d(0, 0, new Rotation2d(0)));
@@ -170,12 +192,146 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+    SmartDashboard.putNumber("LMVolt", getLeftMasterVel());
+  }
 
   public static void runResetCommand() {
     drive.resetEncoders();
     drive.resetYaw();
     drive.resetXY();
+  }
+
+  public void reportToSmartDashboard() {
+
+  }
+
+  public static void resetGyro() {
+    m_nav.reset();
+  }
+
+  public double getLeftMasterVoltage() {
+    return leftMaster.getVoltage();
+  }
+
+  public double getLeftMasterCurrent() {
+    return leftMaster.getCurrent();
+  }
+
+  public double getLeftMasterVel() {
+    return leftMaster.getVelocity();
+  }
+
+  public double getLeftMasterPos() {
+    return leftMaster.getPosition();
+  }
+
+  public double getRightMasterVoltage() {
+    return rightMaster.getVoltage();
+  }
+
+  public double getRightMasterCurrent() {
+    return rightMaster.getCurrent();
+  }
+
+  public double getRightMasterVel() {
+    return rightMaster.getVelocity();
+  }
+
+  public double getRightMasterPos() {
+    return rightMaster.getPosition();
+  }
+
+  public double getLeftFollowerVoltage() {
+    return leftFollower.getVoltage();
+  }
+
+  public double getLeftFollowerCurrent() {
+    return leftFollower.getCurrent();
+  }
+
+  public double getLeftFollowerVel() {
+    return leftFollower.getVelocity();
+  }
+
+  public double getLeftFollowerPos() {
+    return leftFollower.getPosition();
+  }
+
+  public double getRightFollowerVoltage() {
+    return rightFollower.getVoltage();
+  }
+
+  public double getRightFollowerCurrent() {
+    return rightFollower.getCurrent();
+  }
+
+  public double getRightFollowerVel() {
+    return rightFollower.getVelocity();
+  }
+
+  public double getRightFollowerPos() {
+    return rightFollower.getPosition();
+  }
+
+  public double getElevatorVoltage() {
+    return elevator.getVoltage();
+  }
+
+  public double getElevatorCurrent() {
+    return elevator.getCurrent();
+  }
+
+  public double getElevatorVel() {
+    return elevator.getVelocity();
+  }
+
+  public double getElevatorPos() {
+    return elevator.getPosition();
+  }
+
+  public double getArmVoltage() {
+    return arm.getVoltage();
+  }
+
+  public double getArmCurrent() {
+    return arm.getCurrent();
+  }
+
+  public double getArmVel() {
+    return arm.getVelocity();
+  }
+
+  public double getArmPos() {
+    return arm.getPosition();
+  }
+
+  public double getCurrentRoll() {
+    return m_nav.getRoll();
+  }
+
+  public double getCurrentPitch() {
+    return m_nav.getPitch();
+  }
+
+  public double getCurrentYaw() {
+    return m_nav.getYaw();
+  }
+
+  public double getNavTimestamp() {
+    	return m_nav.getLastSensorTimestamp();
+  }
+
+  public double getCurrentAccelX() {
+	    return m_nav.getWorldLinearAccelX();
+  }
+
+  public double getCurrentAccelY() {
+    return m_nav.getWorldLinearAccelY();
+  }
+
+  public double getCurrentAccelZ() {
+    return m_nav.getWorldLinearAccelZ();
   }
 
   public void startLog(){
@@ -207,7 +363,13 @@ public class Robot extends TimedRobot {
       }
       try {
         m_writer = new FileWriter(m_file);
-        m_writer.append(csq);
+        m_writer.append("Time, Match Time, Pitch, Roll, Yaw" + "X Accel, Y Accel, Z Accel" + 
+                "Left Master Voltage, Left Master Current, Right Master Voltage, Right Master Current" +
+                "Left Follower Voltage, Left Follower Current, Right Follower Current, Right Follower Current" +
+                "Left Master Vel, Left Master Pos, Right Master Vel, Right Master Pos" +
+                "Left Follower Vel, Left Follower Pos, Right Follower Vel, Right Follower Pos" +
+                "Elevator Voltage, Elevator Current, Elevator Velocity, Elevator Position" + 
+                "Arm Voltage, Arm Current, Arm Velocity, Arm Position/n");
         m_logStartTime = Timer.getFPGATimestamp();
       } catch (IOException e) {
         e.printStackTrace();
@@ -232,12 +394,27 @@ public class Robot extends TimedRobot {
     if (!writeException) {
       try {
         double timestamp = Timer.getFPGATimestamp() - m_logStartTime;
-        m_writer.append(csq);
+        m_writer.append(String.valueOf(timestamp) + "," + String.valueOf(Robot.ds.getMatchTime()) + "," 
+                + String.valueOf(getCurrentPitch()) + "," + String.valueOf(getCurrentRoll()) + ","
+                + String.valueOf(getCurrentYaw()) + "," + String.valueOf(getCurrentAccelX()) + ","
+                + String.valueOf(getCurrentAccelY()) + "," + String.valueOf(getCurrentAccelZ()) + ","
+                + String.valueOf(getLeftMasterVoltage()) + "," + String.valueOf(getLeftMasterCurrent()) + ","
+                + String.valueOf(getRightMasterVoltage()) + "," + String.valueOf(getRightMasterCurrent()) + ","
+                + String.valueOf(getLeftFollowerVoltage()) + "," + String.valueOf(getLeftFollowerCurrent()) + ","
+                + String.valueOf(getRightFollowerVoltage()) + "," + String.valueOf(getRightFollowerCurrent()) + ","
+                + String.valueOf(getLeftMasterVel()) + "," + String.valueOf(getLeftMasterPos()) + ","
+                + String.valueOf(getRightMasterVel()) + "," + String.valueOf(getRightMasterPos()) + ","
+                + String.valueOf(getLeftFollowerVel()) + "," + String.valueOf(getLeftFollowerPos()) + ","
+                + String.valueOf(getRightFollowerVel()) + "," + String.valueOf(getRightFollowerPos()) + ","
+                + String.valueOf(getElevatorVoltage()) + "," + String.valueOf(getElevatorCurrent()) + ","
+                + String.valueOf(getElevatorVel()) + "," + String.valueOf(getElevatorPos()) + ","
+                + String.valueOf(getArmVoltage()) + "," + String.valueOf(getArmCurrent()) + ","
+                + String.valueOf(getArmVel()) + "," + String.valueOf(getArmPos()) + ",");
       } catch (IOException e) {
         e.printStackTrace();
         writeException = true;
       }
     }
   }
-  
+
 }
