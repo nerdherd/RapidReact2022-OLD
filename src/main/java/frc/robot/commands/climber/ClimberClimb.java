e396c81035dbb9ea9e296de306fd23204c65646c
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2022-02-12 09:17:08 -0800

    Add climber commands

---
+++ src/main/java/frc/robot/commands/climber/ClimberClimb.java
@@ -0,0 +1,39 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+/* In order to run this command, position robot so it is relatively close to 
+bottom rung facing the hangar. The robot will drive forward until a curret 
+change in arm is detected. */
+
+package frc.robot.commands.climber;
+
+import frc.robot.Robot;
+import frc.robot.constants.ClimberConstants;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public class ClimberClimb extends SequentialCommandGroup{
+    
+    // Robot.arm.detectCurrent() should have isFinished return true if current goes out of range
+
+    public ClimberClimb() {
+        for (int i = 0; i < 3; i++) {
+            addCommands(
+                new InstantCommand(Robot.arm.setAngle(ClimberConstants.kArmRotBack)),
+                new InstantCommand(Robot.armElevator.setHeight(ClimberConstants.kArmTravelUp)),
+                new ParallelRaceGroup(Robot.arm.setAngle(ClimberConstants.kArmRotForwards), Robot.arm.detectCurrent()),
+                new InstantCommand(Robot.armElevator.setHeight(ClimberConstants.kArmTravelDown))
+            );
+        }
+    }
+
+}
--- src/main/java/frc/robot/constants/ClimberConstants.java
+++ src/main/java/frc/robot/constants/ClimberConstants.java
@@ -11,29 +11,39 @@ package frc.robot.constants;
  * Add your docs here.
  */
 public class ClimberConstants {
-    public static final double kClimberTalonDeadband = 0.004;
-    public static final double kClimberDistanceRatio = 1. / 4096. * 1.432 * Math.PI;
-    public static final double kClimberHeightOffset = 2 + 7.5;
-    public static final double kClimberDesiredLiftPow = -0.75;
-    public static final double kClimberDesiredUpPow = 0;
-    public static final double kClimberDesiredHoldPow = -0.1;
-    public static final double kHardStopPos = 740000;
-    public static final double kClimbGoodPos = 200000;
+    public static final double kArmReadyAngle = 0; // Angle of arm before lifting robot off ground, should go backwards
+    public static final double kElevatorReadyPosition = 0;
+    public static final double kElevatorLiftPosition = 0;
+    public static final double kApproachSpeed = 0; // Speed drivetrain approaches bar
+    public static final double kMinArmHitCurrent = 0; // Threshold passed when arm collides with an object
+    public static final double kArmRotBack = 0;
+    public static final double kArmRotForwards = 0; // Should be slightly more than needed because current detection is also used
+    public static final double kArmTravelUp = 0;
+    public static final double kArmTravelDown = 0;
 
-    public static final double kArmAngleRatio = 0;
-    public static final double kArmAngleOffset = 0;
-    public static final double kArmVel = 0;
-    public static final double kArmAccel = 0;
-    public static final double kArmP = 0;
-    public static final double kArmF = 0;
-    public static final double kArmGravityFF = 0;
-    public static final double kArmStaticFriction = 0;
-    public static final double kArmS = 0;
-    public static final double kArmCos = 0;
-    public static final double kArmV = 0;
-    public static final double kArmA = 0;
-    public static final int kMotionMagicAcceleration = 0;
-    public static final int kMotionMagicVelocity = 0;
+    // public static final double kClimberTalonDeadband = 0.004;
+    // public static final double kClimberDistanceRatio = 1. / 4096. * 1.432 * Math.PI;
+    // public static final double kClimberHeightOffset = 2 + 7.5;
+    // public static final double kClimberDesiredLiftPow = -0.75;
+    // public static final double kClimberDesiredUpPow = 0;
+    // public static final double kClimberDesiredHoldPow = -0.1;
+    // public static final double kHardStopPos = 740000;
+    // public static final double kClimbGoodPos = 200000;
 
-    public static final double kRungAngle = 0.568977336;
+    // public static final double kArmAngleRatio = 0;
+    // public static final double kArmAngleOffset = 0;
+    // public static final double kArmVel = 0;
+    // public static final double kArmAccel = 0;
+    // public static final double kArmP = 0;
+    // public static final double kArmF = 0;
+    // public static final double kArmGravityFF = 0;
+    // public static final double kArmStaticFriction = 0;
+    // public static final double kArmS = 0;
+    // public static final double kArmCos = 0;
+    // public static final double kArmV = 0;
+    // public static final double kArmA = 0;
+    // public static final int kMotionMagicAcceleration = 0;
+    // public static final int kMotionMagicVelocity = 0;
+
+    // public static final double kRungAngle = 0.568977336;
 }
